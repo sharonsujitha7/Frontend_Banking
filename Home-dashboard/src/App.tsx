c5e637dc5315140f141c289593eb0f6d38548002
Gayathri Magesh <54061255+gayathrimagesh@users.noreply.github.com>
2023-08-28 14:01:34 +0530

    Add files via upload

---
+++ Home-dashboard/src/App.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/layout/MainLayout";
+import { routes } from "./routes";
+import Home from './components/Home';
+import CreateAccount from './CreateAcoount';
+import SignIn from './Login';
+import Register from './Register';
+import ForgotUserName from './ForgotUsername';
+import ForgotPassword from './ForgotPassword';
+import ResetPassword from './ResetPassword';
+import AddPayee from './AddPayee';
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+      <Routes>
+        
+        <Route path="/" element={<Home/>}/>
+            <Route path="/createAccount" element={<CreateAccount/>}/>
+            <Route path="/login" element={<SignIn/>}/>
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/forgotUserName" element={<ForgotUserName/>}/>
+            <Route path="/forgotPassword" element={<ForgotPassword/>}/> 
+            
+            <Route path="/resetpassword" element ={<ResetPassword/>}/>
+            {/* <Route path="/addpayee" element={<AddPayee/>}/> */}
+            <Route path="/dashboard" element={<MainLayout />}>
+          {routes}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ Home-dashboard/src/pages/AccountStatement.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AccountStatement(){
+    return(
+    <div>
+        <h1><b>Account Statement</b></h1>
+        <p> Account Number : </p>
+        <p> Balance: </p>
+        <p> Date of opening: </p>
+        
+    </div>
+    )
+}
+export default AccountStatement;
